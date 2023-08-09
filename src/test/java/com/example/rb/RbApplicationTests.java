59bed7e144323e7efa4177420946a60627a02ec1
jyk <realzero101@gmail.com>
2023-08-09 16:36:40 +0900

    initial commit

---
+++ src/test/java/com/example/rb/RbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
