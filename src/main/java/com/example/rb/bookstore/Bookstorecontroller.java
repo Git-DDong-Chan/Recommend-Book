d8ac28820c56132a96a4d9b8296e77d5b5eeed19
jyk <realzero101@gmail.com>
2023-08-24 17:35:54 +0900

    Update

--- src/main/java/com/example/rb/bookstore/Bookstorecontroller.java
+++ src/main/java/com/example/rb/bookstore/Bookstorecontroller.java
@@ -1,6 +1,6 @@
 package com.example.rb.bookstore;
 
-import java.util.List;
+
 
 
 import org.springframework.stereotype.Controller;
@@ -14,24 +14,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.rb.user.UserService;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @RequiredArgsConstructor
 @Controller
 public class Bookstorecontroller {
-
+    
     private final BookstoreService bookstoreservice;
     private final UserService userService;
 
     
-  
+   @PreAuthorize("isAuthenticated()")
     @GetMapping("/bookstore/list")
     public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,@RequestParam(value = "kw", defaultValue = "") String kw) {
+        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUsername = authentication.getName();
         Page<bookentity> paging =this.bookstoreservice.getList(page,kw);
+
         model.addAttribute("paging", paging);
         model.addAttribute("kw", kw);
         model.addAttribute("loggedInUsername", loggedInUsername);
         return "bookstorelist";
     }
+
+   
     
   
 }
+
