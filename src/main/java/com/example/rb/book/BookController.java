a11250fa47e900cf12ffa182b6136c93aea0332d
jyk <realzero101@gmail.com>
2023-08-27 15:50:04 +0900

    update main

--- src/main/java/com/example/rb/book/BookController.java
+++ src/main/java/com/example/rb/book/BookController.java
@@ -1,5 +1,6 @@
 package com.example.rb.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 
